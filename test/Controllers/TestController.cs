d8eac14965c5c67640330ef36e3ff844636c6059
Anders Kjeldsen <ande7353@gmail.com>
2016-12-27 00:03:26 +0100

    Initial commit

---
+++ test/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace test.Controllers
+{
+    public class TestController : Controller
+    {
+        private static readonly object Mylock = new object();
+        private const string Path = "~/Content/count.txt";
+        public ActionResult Test()
+        {
+            ulong count;
+            lock (Mylock)
+            {
+                var filepath = Server.MapPath(Path);
+                var txt = System.IO.File.ReadAllText(filepath);
+                ulong.TryParse(txt, out count);
+                count++;
+                System.IO.File.WriteAllText(filepath, $"{count}");
+            }
+            return Content($"{count}");
+        }
+    }
+}
---
+++ test/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace test
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            RouteTable.Routes.MapRoute("Default", "", new { controller = "Test", action = "Test" });
+        }
+    }
+}
